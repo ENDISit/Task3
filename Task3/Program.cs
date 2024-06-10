b9e101b29be100b7bad061744e99e40df5813565
ENDISit <mr.ilia08@mail.ru>
2024-06-11 02:02:37 +0300

    Добавьте файлы проекта.

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int minNum = 5;
+            int maxNum = 10;
+            Console.Write("Ввидите число 5 или 10: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (num == minNum)
+            {
+                Console.WriteLine("Число 5 равняется 5.");
+            }
+            else if (num == maxNum)
+            {
+                Console.WriteLine("Число 10 равняется 10.");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
